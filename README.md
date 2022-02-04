9aa0f73857f1c448b6add5cd8e3c305ca646b2fd
Ankur Seth <iankurseth@gmail.com>
2022-02-04 19:58:07 +0530

    Added button for choosing random color * added transparent color option

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Reactry's `chessboard` is wallpaper generator written in `React`.
 
 ## `1.1.0` - `future`
 * added new colors `pink`, `indogo` and `lime`
+* added button to choose a random color
+* added `transparent` color option
 
 <img src="https://raw.githubusercontent.com/reactry/chessboard/master/img/1.0.0.png">
 
--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -5,6 +5,8 @@ export default function ColorPicker ({
 	colors, title, index, setIndex
 }) {
 
+	const maxColors = colors.length;
+
 	let colorItems = colors.map((v, i) => {
 		let borderClass = (i === index) ? "border-4 border-black" : "border-y-4";
 		let classNames = `w-8 h-8 mr-1 cursor-pointer bg-slate-600 ${colors[i]} ${borderClass}`;
@@ -13,9 +15,18 @@ export default function ColorPicker ({
 		);
 	});
 
+	function pickRandomColor (e) {
+		setIndex(Math.floor(Math.random() * maxColors));
+	}
+
+	let randomizeButton = <span className="text-sm px-2 py-1 ml-2 bg-slate-400 cursor-pointer duration-300 hover:bg-slate-600 hover:text-white" onClick={pickRandomColor}>random</span>;
+
 	return (
-		<div className="ColorPicker mb-4">
-			<h2 className="text-sm font-bold mb-1">{title}</h2>
+		<div className="ColorPicker mb-6">
+			<h2 className="font-bold mb-1">
+				<span className="text-sm">{title}</span>
+				{randomizeButton}
+			</h2>
 			<div className="flex">
 				{colorItems}
 			</div>
