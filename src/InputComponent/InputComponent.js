297a14c7afaf748bfdf532a0dd1a1978a12ef508
Ankur Seth <iankurseth@gmail.com>
2022-02-05 12:55:21 +0530

    Moved resolutions selector to SizeTab

--- src/InputComponent/InputComponent.js
+++ src/InputComponent/InputComponent.js
@@ -27,16 +27,6 @@ export default function InputComponent ({
 	let [showDebugTable, setShowDebugTable] = React.useState(true);
 
 	let resolution = resolutions[resolutionIndex];
-	let width = resolution.width;
-	let height = resolution.height;
-
-	let resolutionItems = resolutions.map((v, i) => {
-		let borderClass = (i === resolutionIndex) ? "border-y-4 border-red-500" : "border-y-4";
-		let classNames = `px-3 py-1 mr-1 cursor-pointer bg-slate-600 ${borderClass}`;
-		return (
-			<div className={classNames} key={i} onClick={(e) => setResolutionIndex(i)}>{v.name}</div>
-		);
-	});
 
 	function downloadPng (e) {
 		const filename = `reactry_${resolution.name}.png`;
@@ -87,14 +77,7 @@ export default function InputComponent ({
 	return (
 		<div className="InputComponent">
 			<TopTabBar {...topTabBarProps} />
-			<div className="flex px-4 py-2 text-white font-bold text-sm">
-				{resolutionItems}
-				<div className="px-4 py-2 ml-4 bg-rose-500 inline-block rounded-full">
-					<span>{width}</span>
-					<span className="px-1">x</span>
-					<span>{height}</span>
-				</div>
-			</div>
+
 			<div className="p-4">
 				{currentTab()}
 
--- src/InputComponent/SizeTab.js
+++ src/InputComponent/SizeTab.js
@@ -18,6 +18,17 @@ export default function SizeTab ({
 		setHeightIndex(x);
 	};
 
+	let resolution = resolutions[resolutionIndex];
+	let width = resolution.width;
+	let height = resolution.height;
+	let resolutionItems = resolutions.map((v, i) => {
+		let borderClass = (i === resolutionIndex) ? "border-y-4 border-red-500" : "border-y-4";
+		let classNames = `px-3 py-1 mr-1 cursor-pointer bg-slate-600 ${borderClass}`;
+		return (
+			<div className={classNames} key={i} onClick={(e) => setResolutionIndex(i)}>{v.name}</div>
+		);
+	});
+
 	let selectShapeProps = {
 		show: selectSquare,
 		setShow: setSelectSquare,
@@ -52,6 +63,14 @@ export default function SizeTab ({
 
 	return (
 		<div className="SizeTab">
+			<div className="flex py-2 text-white font-bold text-sm">
+				{resolutionItems}
+				<div className="px-4 py-2 ml-4 bg-rose-500 inline-block rounded-full">
+					<span>{width}</span>
+					<span className="px-1">x</span>
+					<span>{height}</span>
+				</div>
+			</div>
 			<ShowToggle {...selectShapeProps} />
 			{shapeSelector()}
 			<NumberPicker title="Border size"
